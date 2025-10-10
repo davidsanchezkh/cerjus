8ed0bc93e737cec923dce1b9be80ef27ba89b72d
Sanchez321 <davidsanchezgarcia@live.com>
2025-10-09 21:22:06 -0500

    feat: set API_URL to Koyeb in prod

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,25 @@
+// app.config.ts
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { routes } from './app.routes';
-
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { API_URL } from './app.token';
-
+import { API_URL } from './app.token'
 import { jwtInterceptor } from './interceptor/interceptor.auth';
+
+const isProd =
+  typeof window !== 'undefined' &&
+  location.hostname === 'davidsanchezkh.github.io';
+
+const apiBase = isProd
+  ? 'https://spiritual-marie-jeanne-cerjus-caf28ccd.koyeb.app/cerjusbackend'
+  : 'http://localhost:3000'; // tu backend local
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([jwtInterceptor])
-    ),
-    {provide:API_URL, useValue:'http://localhost:3000'}
+    provideHttpClient(withInterceptors([jwtInterceptor])),
+    { provide: API_URL, useValue: apiBase }
   ]
 };
